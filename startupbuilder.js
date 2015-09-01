b14203c95f590f2371eecdd20dc03b7f33068fb7
Guillaume Badi <gbadi@mailjet.com>
2015-09-01 16:57:03 +0200

    startup builder

---
+++ startupbuilder.js
@@ -0,0 +1,17 @@
+
+import fs from 'fs';
+
+export default class StratupBuilder {
+
+  constructor () {
+    this.file = fs.readFileSync('./README.md');
+  }
+
+  getRandomCompany () {
+
+  }
+
+  getRandomDomain () {
+
+  }
+}
